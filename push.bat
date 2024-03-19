32711fa4240f4d3c487b80c289f77a5981d976f7
The Codesmith <b-9k@outlook.com>
2024-03-19 10:48:43 -0400

    .

--- push.bat
+++ push.bat
@@ -1,5 +1,12 @@
+@echo off
+echo ==================================== START
 git diff
+echo ************************
 git add .
+echo ************************
 git commit
+echo ************************
 git push
+echo ==================================== DONE
+echo.
 pause
